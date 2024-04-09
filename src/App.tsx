3a7b4e38c8cd9555c87d3f1b6619117484860ff2
tish-code <eliastish2003@gmail.com>
2024-04-09 16:35:47 +0100

    project pause

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const data: Array<IActivity> = [
   { month: 11, rate: 35 },
   { month: 12, rate: 10 },
 ];
+
 function App() {
   const [info, setInfo] = useState(data);
   const [isSorting, setIsSorting] = useState(false);
@@ -47,7 +48,6 @@ function App() {
   }, [info]);
 
   useEffect(() => {
-    console.log("hello");
     const animationTimeout = setTimeout(() => {
       setAnimateBars(false);
     }, 2000);
@@ -55,8 +55,8 @@ function App() {
     return () => clearTimeout(animationTimeout); // sets animateBars to false so it only animate only on initial page render
   }, []);
   return (
-    <main className="py-[3rem] h-screen bg-slate-50">
-      <div className="ml-auto mr-auto rounded-xl shadow-xl bg-white p-4 max-w-[700px]">
+    <main className="py-[3rem] min-h-screen">
+      <div className="rounded-xl bg-indigo-50 p-4 w-[600px] m-auto">
         <div className="flex justify-between items-end border-[0] border-gray-300 border-l-2 border-b-2 h-[400px] px-1">
           {info.map((item: IActivity, index) => {
             return (
@@ -67,11 +67,13 @@ function App() {
                   height: `${item.rate}%`,
                   order: index,
                 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
                 {...(animateBars && {
                   transition: {
                     type: "spring",
                     duration: 1,
-                    delay: index > 4 ? 0.6 : index > 8 ? 0.9 : 0.3,
+                    delay: index > 5 ? 0.6 : 0.3,
                   },
                   initial: { height: "auto" },
                   animate: { height: `${item.rate}%` },
